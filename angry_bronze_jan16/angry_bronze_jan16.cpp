5df85e8873beac93ce09d7aa666bafb44fd2049c
Ben Wu <benwu@Bens-MacBook-Air.local>
2023-06-28 15:07:26 -0500

    done

--- angry_bronze_jan16/angry_bronze_jan16.cpp
+++ angry_bronze_jan16/angry_bronze_jan16.cpp
@@ -3,13 +3,6 @@
 #include <set>
 using namespace std;
 
-void print_vector(vector<int> vec) {
-    for (auto it : vec){
-        cout << it << " ";
-    }
-    cout << endl;
-}
-
 int main() {
     int n;
     cin >> n;
@@ -26,37 +19,36 @@ int main() {
     }
 
     sort(cows.begin(), cows.end());
-    // print_vector(cows);
+
     for (auto cow : cows){
         vector<int> blasted = {cow};
-        // cout << "TESTING " << cow << endl;
+
+        bool left = true;
+        bool right = true;
+
         for (int i = 1; i < n + 1; i ++){
-            // cout << "blast " << i << endl;
-            bool left = true;
-            bool right = true;
+            
             int blast_start = blasted[0];
             int blast_end = blasted[blasted.size() - 1];
-            // cout << "START " << blast_start << endl;
-            // cout << "END " << blast_end << endl; 
+
             for (int x = 1; x <= i; x ++){
-                // cout << "range " << x << endl; 
                 if (find(cows.begin(), cows.end(), blast_start - x) != cows.end() && left){
-                    // cout << "found " << (blast_start - x) << endl;
                     blasted.insert(blasted.begin(), blast_start - x);
                 }
-                // cout << blasted[-1] + x << endl;
+                
                 if (find(cows.begin(), cows.end(), blast_end + x) != cows.end() && right){
-                    // cout << "found " << (blast_end + x) << endl;
                     blasted.push_back(blast_end + x);
-                } 
+                }
                 
                 
             }
             if (blast_start == blasted[0] && blast_end == blasted[blasted.size() - 1]) break;
+            if (blast_start == blasted[0]) left = false;
+            if (blast_end == blasted[blasted.size() - 1]) right = false;
             
         }
         max_blasted = max(max_blasted, int(blasted.size()));
-        // print_vector(blasted); 
+
     }
     cout << max_blasted << endl;
 }
--- kayaking/kayaking.cpp
+++ kayaking/kayaking.cpp
@@ -4,23 +4,51 @@
 #include <map>
 using namespace std;
 
+void print_vector(vector<int> vec) {
+    for (auto it : vec){
+        cout << it << " ";
+    }
+    cout << endl;
+}
+
+int find_best(vector<int> people1, int prev_singles) {
+    // print_vector(people1);
+    // cout << prev_singles << endl;
+    if (prev_singles == 0 && people1.size() == 2){
+        return people1[1] - people1[0];
+    } 
+    else if (people1.size() == 1) return 0;
+    else if (prev_singles == 0) return (people1[1] - people1[0]) + find_best(vector<int> (people1.begin() + 2, people1.end()), 0);
+    else if (prev_singles == 2 && people1.size() == 2) return 0;
+    else{
+        return min(people1[1] - people1[0] + find_best(vector<int> (people1.begin() + 2, people1.end()), prev_singles),
+        find_best(vector<int> (people1.begin() + 1, people1.end()), prev_singles - 1));
+    }
+}
+
 int main() {
     int n;
     cin >> n;
 
-    int min_instability = 0;
+    
 
     vector <int> people;
-    vector <int> differences;
+    // vector <int> differences;
     for (int i = 0; i < n * 2; i ++){
         int person;
         cin >> person;
         people.push_back(person);
-        if (i > 0){
-            differences.push_back(person - people[i - 1]);
-        }
+        // if (i > 0){
+        //     differences.push_back(person - people[i - 1]);
+        // }
     }
-    
+
+    sort(people.begin(), people.end());
+
+    // print_vector(people);
+
+
+    cout << find_best(people, 2) << endl;
     
     
 
@@ -61,15 +89,15 @@ int main() {
     //         }
     //     }
     // }
-    for (int i = 0; i < n - 1; i ++){
-        pair<int, int> min_dif = make_pair(INT32_MAX, 0);
-        for (int x = 0; x < (n * 2) - 1; x ++){
-            if (differences[x] < min_dif.first){
-                min_dif.first = differences[x]
-            } 
-        }
-    }
+    // for (int i = 0; i < n - 1; i ++){
+    //     pair<int, int> min_dif = make_pair(INT32_MAX, 0);
+    //     for (int x = 0; x < (n * 2) - 1; x ++){
+    //         if (differences[x] < min_dif.first){
+    //             min_dif.first = differences[x]
+    //         } 
+    //     }
+    // }
 
-    cout << min_instability;
+    // cout << min_instability;
 
 }
