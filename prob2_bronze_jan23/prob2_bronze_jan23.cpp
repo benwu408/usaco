c9b83247f1e171b072968404fa714917a081c106
jasmine2000 <jasmine2000@gmail.com>
2023-07-05 21:05:06 -0500

    retire and edit

--- prob2_bronze_jan23/prob2_bronze_jan23.cpp
+++ prob2_bronze_jan23/prob2_bronze_jan23.cpp
@@ -5,64 +5,76 @@
 #include <string>
 using namespace std;
 
-vector<string> perms;
+struct Cow {
+    int start;
+    int end;
+    int cool;
+};
 
-bool is_valid(vector<vector<int> >cows, string used_ac, vector<vector<int> > acs){
-    vector<int> temps(101);
+struct AC {
+    int start;
+    int end;
+    int cool;
+    int price;
+};
+
+bool is_valid(vector<Cow >cows, string used_ac, vector<AC > acs){
+    vector<int> temps(100);
     for (int i = 0; i < used_ac.size(); i ++){
         if (used_ac[i] == '1'){
-            for (int a = acs[i][0]; a <= acs[i][1]; a ++){
-                temps[a] += acs[i][2];
+            for (int a = acs[i].start; a <= acs[i].end; a ++){
+                temps[a - 1] += acs[i].cool;
             }
         }
     }
     for (auto cow : cows){
-        for (int i = cow[0]; i <= cow[1]; i ++){
-            if (temps[i] < cow[2]) return false;
+        for (int i = cow.start; i <= cow.end; i ++){
+            if (temps[i - 1] < cow.cool) return false;
         }
     }
     return true;
 }
-void search(string current, int m){
-    string next = current;
-    if (current.size() == m) perms.push_back(next);
-    else{
-        search(next + "1", m);
-        search(next + "0", m);
+
+vector<string> search(string current, int m){
+    vector<string> result;
+    if (current.size() == m) return {current};
+
+    for (string a : {"1", "0"}) {
+        auto recurse = search(current + a, m);
+        result.insert(result.end(), recurse.begin(), recurse.end());
     }
+
+    return result;
 }
 
 int main(){
     int n, m;
     cin >> n >> m;
 
-    vector<vector<int> > cows;
-    vector<vector<int> > acs;
+    vector<Cow > cows;
+    vector<AC > acs;
 
     for (int i = 0; i < n; i ++){
         int s, t, c;
         cin >> s >> t >> c;
 
-        cows.push_back({s, t, c});
+        cows.push_back(Cow {s, t, c});
     }
-
     for (int i = 0; i < m; i ++){
         int a, b, p, m;
         cin >> a >> b >> p >> m;
 
-        acs.push_back({a, b, p, m});
+        acs.push_back(AC{a, b, p, m});
     }
 
-    search("", m);
-    // for (auto it : perms) cout << it << endl;
+    auto perms = search("", m);
     int min_cost = INT32_MAX;
 
     for (auto perm : perms){
         if (is_valid(cows, perm, acs)){
-            // cout << perm << endl;
             int cost = 0;
             for (int i = 0; i < m; i ++){
-                if (perm[i] == '1') cost += acs[i][3];
+                if (perm[i] == '1') cost += acs[i].price;
             }
             min_cost = min(cost, min_cost);
         }
