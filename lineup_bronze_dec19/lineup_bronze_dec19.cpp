b7e2e1bc9ca7ea1135ef11611e5c01deb61cdcce
Ben Wu <benwu@Bens-MacBook-Air.local>
2023-07-13 21:35:24 -0500

    not work

--- lineup_bronze_dec19/lineup_bronze_dec19.cpp
+++ lineup_bronze_dec19/lineup_bronze_dec19.cpp
@@ -1,49 +1,23 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <set>
+#include <map>
+#include <string>
 using namespace std;
 
-void print_vector(vector<vector<vector<string> > > vec){
-    cout << "printing vec" << endl;
-    for (auto it : vec){
-        for (auto itr : it){
-            for (auto iter : itr){
-                cout << iter << " ";
-            }
-            cout << endl;
-        }
-        cout << endl;
-        
-    }
-    
-    
+void print_vector(vector<string> vec){
+    cout << "vector : ";
+    for (auto str : vec) cout << str << " ";
+    cout << endl << endl;
 }
 
-bool satisfies(vector<string> vec, vector<pair<string, string> > partners){
-    for (int i = 0; i < 8; i ++){
-        for (auto it : partners){
-            if (it.first == vec[i]){
-                bool valid = false;
-                if (i > 0){
-                    if (vec[i - 1] == it.second) valid = true;
-                }
-                if (i < 7){
-                    if (vec[i + 1] == it.second) valid = true;
-                }
-                if (!valid) return false;
-            }
-            
-        }
-    }
-    return true;
-}
 int main() {
     vector<string> cows = {"Beatrice", "Belinda", "Bella", "Bessie", "Betsy", "Blue", "Buttercup", "Sue"};
-    set<string> previous;
-    sort(cows.begin(), cows.end());
-    // print_vector(cows);
-    vector<pair<string, string> > pairs;
+    map<string, vector<string> > cowMap;
+
+    for (auto cow : cows) cowMap[cow] = {};
+
+
     int n;
     cin >> n;
 
@@ -56,21 +30,65 @@ int main() {
             if (x == 0) cow1 = word;
             if (x == 5) cow2 = word;
         }
-        pairs.push_back(make_pair(min(cow1, cow2), max(cow1, cow2)));
+        cowMap[cow1].push_back(cow2);
+        cowMap[cow2].push_back(cow1);
     }
-    sort(pairs.begin(), pairs.end());
-    if (satisfies(cows, pairs)){
-        for (auto it : cows) cout << it << endl;
-    }
-    else{
-        while (next_permutation(cows.begin(), cows.end())){
-            if (satisfies(cows, pairs)){
-                for (auto it : cows) cout << it << endl;
-                break;
-            }
+
+    vector<string> new_cows;
+
+    while (cows.size() > 0){
+        cout << cows[0] << endl;
+        print_vector(new_cows);
+        string curCow = cows[0];
+        if (new_cows.size() > 0 && cowMap[*(new_cows.end() - 1)].size() == 1){
+            string lastCow = *(new_cows.end() - 1);
+            cout << "poooo" << endl;
+            auto newCow = cowMap[lastCow][0];
+            cout << newCow << endl;
+            new_cows.push_back(newCow);
+            auto newCowPtr = find(cows.begin(), cows.end(), newCow);
+            cout << *newCowPtr << endl;
+            cows.erase(newCowPtr);
+            cowMap[lastCow] = {};
+            cowMap[newCow].erase(find(cowMap[newCow].begin(), cowMap[newCow].end(), lastCow));
         }
+        else {
+            cout << "still poop" << endl;
+            vector<string> connections = cowMap[curCow];
+            int num_connect = connections.size();
+            if (num_connect < 2){
+                cout << "poop" << endl;
+                new_cows.push_back(curCow);
+                cows.erase(cows.begin());
+            }
+            else {
+                auto cow1 = min(connections[0], connections[1]);
+                auto cow2 = max(connections[0], connections[1]);
+                if (cowMap[cow1].size() == 1) {
+                    new_cows.push_back(cow1);
+                    new_cows.push_back(curCow);
+                    new_cows.push_back(cow2);
+                    cowMap[cow1] = {};
+                    cowMap[cow2].erase(find(cowMap[cow2].begin(), cowMap[cow2].end(), curCow));
+                    
+                }
+                else {
+                    new_cows.push_back(cow2);
+                    new_cows.push_back(curCow);
+                    new_cows.push_back(cow1);
+                    cowMap[cow1].erase(find(cowMap[cow1].begin(), cowMap[cow1].end(), curCow));
+                    cowMap[cow2] = {};
+                }
+                cows.erase(cows.begin());
+                cows.erase(find(cows.begin(), cows.end(), cow1));
+                cows.erase(find(cows.begin(), cows.end(), cow2));
+                cowMap[curCow] = {};
+            }
+
+        }   
+        
     }
-    
-    
+    for (auto cow : new_cows) cout << cow << endl;
     
 }
+
